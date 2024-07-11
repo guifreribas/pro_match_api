50c8ae8b9d9a3ddeee431e4a0841406a72c5da95
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-11 10:20:30 +0200

    feat: Add competition controller functions
    This commit adds the necessary controller functions for performing CRUD operations on competitions. The `getCompetitions` function retrieves all competitions from the database, `getCompetition` retrieves a specific competition by its ID, `createCompetition` creates a new competition, `updateCompetition` updates an existing competition, and `deleteCompetition` deletes a competition. These functions enhance the application by allowing users to manage their competition data effectively.

---
+++ src/controllers/competitionController.js
@@ -0,0 +1,103 @@
+export const getCompetitions = async (req, res) => {
+    try {
+        const competitions = await Competition.findAll();
+        res.status(200).json({
+            success: true,
+            message: "Competitions fetched successfully",
+            data: competitions,
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to get competitions" });
+    }
+};
+
+export const getCompetition = async (req, res) => {
+    try {
+        const competition = await Competition.findByPk(req.params.id);
+        if (competition) {
+            res.status(200).json({
+                success: true,
+                message: "Competition fetched successfully",
+                data: competition,
+                timestamp: new Date().toISOString(),
+            });
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Competition not found",
+                data: {
+                    id_competition: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error to get competition" });
+    }
+};
+
+export const createCompetition = async (req, res) => {
+    try {
+        const competition = await Competition.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: "Competition created successfully",
+            data: competition,
+            links: {
+                self: `${config.API_BASE_URL}/competitions/${competition.id_competition}`,
+            },
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to create competition" });
+    }
+};
+
+export const updateCompetition = async (req, res) => {
+    try {
+        const competition = await Competition.update(req.body, {
+            where: { id_competition: req.params.id },
+        });
+        res.status(200).json({
+            success: true,
+            message: "Competition updated successfully",
+            data: competition,
+            links: {
+                self: `${config.API_BASE_URL}/competitions/${competition.id_competition}`,
+            },
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to update competition" });
+    }
+};
+
+export const deleteCompetition = async (req, res) => {
+    try {
+        const competition = await Competition.destroy({
+            where: { id_competition: req.params.id },
+        });
+        if (competition) {
+            res.status(200).json({
+                success: true,
+                message: "Competition deleted successfully",
+                data: {
+                    id: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Competition not found",
+                data: {
+                    id: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error to delete competition" });
+    }
+};
