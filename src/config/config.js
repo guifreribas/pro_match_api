55ff56888403836684b43571e7476234a5f68ead
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-11 10:19:39 +0200

    feat: Add configuration file for environment variables
    This commit adds a new configuration file, `config.js`, which handles environment variables for the application. It uses the `dotenv` package to load variables from a `.env` file.
    
    The configuration file includes settings for the application's port, host name, database connection, and API base URL. It also provides default values for these settings if they are not specified in the environment.
    
    This change improves the maintainability and flexibility of the application by centralizing the configuration settings in one file.

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+    port: process.env.PORT || 3000,
+    host: process.env.HOST_NAME,
+    database: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        username: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+    },
+    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3000/api",
+};
+
+export default config;
