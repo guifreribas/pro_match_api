9cf89b63b10cdd5328ac2f0120973a61be3b8dee
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-11 10:20:40 +0200

    feat: Add goal controller functions
    This commit adds the necessary controller functions for performing CRUD operations on goals. The `getGoals` function retrieves all goals from the database, `getGoal` retrieves a specific goal by its ID, `createGoal` creates a new goal, `updateGoal` updates an existing goal, and `deleteGoal` deletes a goal. These functions enhance the application by allowing users to manage their goal data effectively.

---
+++ src/controllers/goalController.js
@@ -0,0 +1,103 @@
+export const getGoals = async (req, res) => {
+    try {
+        const goals = await Goal.findAll();
+        res.status(200).json({
+            success: true,
+            message: "Goals fetched successfully",
+            data: goals,
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to get goals" });
+    }
+};
+
+export const getGoal = async (req, res) => {
+    try {
+        const goal = await Goal.findByPk(req.params.id);
+        if (goal) {
+            res.status(200).json({
+                success: true,
+                message: "Goal fetched successfully",
+                data: goal,
+                timestamp: new Date().toISOString(),
+            });
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Goal not found",
+                data: {
+                    id_goal: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error to get goal" });
+    }
+};
+
+export const createGoal = async (req, res) => {
+    try {
+        const goal = await Goal.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: "Goal created successfully",
+            data: goal,
+            links: {
+                self: `${config.API_BASE_URL}/goals/${goal.id_goal}`,
+            },
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to create goal" });
+    }
+};
+
+export const updateGoal = async (req, res) => {
+    try {
+        const goal = await Goal.update(req.body, {
+            where: { id_goal: req.params.id },
+        });
+        res.status(200).json({
+            success: true,
+            message: "Goal updated successfully",
+            data: goal,
+            links: {
+                self: `${config.API_BASE_URL}/goals/${goal.id_goal}`,
+            },
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error to update goal" });
+    }
+};
+
+export const deleteGoal = async (req, res) => {
+    try {
+        const goal = await Goal.destroy({
+            where: { id_goal: req.params.id },
+        });
+        if (goal) {
+            res.status(200).json({
+                success: true,
+                message: "Goal deleted successfully",
+                data: {
+                    id: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Goal not found",
+                data: {
+                    id: req.params.id,
+                },
+                timestamp: new Date().toISOString(),
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error to delete goal" });
+    }
+};
