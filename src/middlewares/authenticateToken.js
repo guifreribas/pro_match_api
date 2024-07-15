133733261f95fc28637508ff1d2201ab07b160a4
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 11:15:59 +0200

    authentication file

---
+++ src/middlewares/authenticateToken.js
@@ -0,0 +1,51 @@
+// src/middlewares/authenticateToken.js
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+export const authenticateToken = (allowedRoles) => async (req, res, next) => {
+    next();
+    return;
+    try {
+        const { cookies } = req;
+        const accessToken = cookies.token;
+
+        if (!accessToken) {
+            return res.status(401).json({
+                code: 401,
+                success: false,
+                message: "No access token provided",
+            });
+        }
+
+        const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+        const user = await User.findByPk(decodedToken.id_user);
+        if (!user) {
+            return res.status(401).json({
+                code: 401,
+                success: false,
+                message: "Token de acceso no válido",
+            });
+        }
+
+        const hasPermission = user.roles.some((role) =>
+            allowedRoles.includes(role)
+        );
+        if (!hasPermission) {
+            return res.status(403).json({
+                code: 403,
+                success: false,
+                message: "You do not have the required permissions.",
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            code: 500,
+            success: false,
+            message: "An error occurred while authenticating the access token",
+        });
+    }
+};
