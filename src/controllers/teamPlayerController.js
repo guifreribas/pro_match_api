947f1b98be65909b6a58e1882dc1812660d9c6a6
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 21:34:37 +0200

    feature: add filters on getTeamPlayers

--- src/controllers/teamPlayerController.js
+++ src/controllers/teamPlayerController.js
@@ -1,12 +1,58 @@
 import TeamPlayer from "../models/teamPlayerModel.js";
+import config from "../config/config.js";
 
 export const getTeamPlayers = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const whereConditions = {};
+    if (req.query.team_id) {
+        whereConditions.team_id = req.query.team_id;
+    }
+    if (req.query.player_id) {
+        whereConditions.player_id = req.query.player_id;
+    }
     try {
-        const teamPlayers = await TeamPlayer.findAll();
+        const { count, rows } = await TeamPlayer.findAndCountAll({
+            where: whereConditions,
+            offset,
+            limit,
+        });
+        const totalPages = Math.ceil(count / limit);
+        const previousLink =
+            page > 1
+                ? `${config.API_BASE_URL}/teamPlayers?page=${page - 1}`
+                : null;
+        const nextLink =
+            page < totalPages
+                ? `${config.API_BASE_URL}/teamPlayers?page=${page + 1}`
+                : null;
+
         res.status(200).json({
             success: true,
             message: "TeamPlayers fetched successfully",
-            data: teamPlayers,
+            data: {
+                items: rows.map((teamPlayer) => ({
+                    id_team_player: teamPlayer.id_team_player,
+                    team_id: teamPlayer.team_id,
+                    player_id: teamPlayer.player_id,
+                })),
+                itemCount: rows.length,
+                totalItems: count,
+                currentPage: page,
+                pageSize: limit,
+                totalPages: totalPages,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+            links: {
+                self: `${config.API_BASE_URL}/teamPlayers?page=${page}`,
+                first: `${config.API_BASE_URL}/teamPlayers?page=1`,
+                last: `${config.API_BASE_URL}/teamPlayers?page=${totalPages}`,
+                next: nextLink,
+                previous: previousLink,
+            },
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
