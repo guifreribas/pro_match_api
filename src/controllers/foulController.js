d4403643e7e8e3c6bb65fadd6b6995e736d50751
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 21:31:35 +0200

    feature: add filters on getFouls

--- src/controllers/foulController.js
+++ src/controllers/foulController.js
@@ -1,12 +1,70 @@
 import Foul from "../models/foulModel.js";
+import config from "../config/config.js";
 
 export const getFouls = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const whereConditions = {};
+    if (req.query.minute) {
+        whereConditions.minute = req.query.minute;
+    }
+    if (req.query.part) {
+        whereConditions.part = req.query.part;
+    }
+    if (req.query.player_id) {
+        whereConditions.player_id = req.query.player_id;
+    }
+    if (req.query.team_id) {
+        whereConditions.team_id = req.query.team_id;
+    }
+    if (req.query.match_id) {
+        whereConditions.match_id = req.query.match_id;
+    }
     try {
-        const fouls = await Foul.findAll();
+        const { count, rows } = await Foul.findAndCountAll({
+            where: whereConditions,
+            offset,
+            limit,
+        });
+        const totalPages = Math.ceil(count / limit);
+        const previousLink =
+            page > 1 ? `${config.API_BASE_URL}/fouls?page=${page - 1}` : null;
+        const nextLink =
+            page < totalPages
+                ? `${config.API_BASE_URL}/fouls?page=${page + 1}`
+                : null;
+
         res.status(200).json({
             success: true,
             message: "Fouls fetched successfully",
-            data: fouls,
+            data: {
+                items: rows.map((foul) => ({
+                    id_foul: foul.id_foul,
+                    minute: foul.minute,
+                    part: foul.part,
+                    player_id: foul.player_id,
+                    team_id: foul.team_id,
+                    match_id: foul.match_id,
+                    createdAt: foul.createdAt,
+                    updatedAt: foul.updatedAt,
+                })),
+                itemCount: rows.length,
+                totalItems: count,
+                currentPage: page,
+                pageSize: limit,
+                totalPages: totalPages,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+            links: {
+                self: `${config.API_BASE_URL}/fouls?page=${page}`,
+                first: `${config.API_BASE_URL}/fouls?page=1`,
+                last: `${config.API_BASE_URL}/fouls?page=${totalPages}`,
+                next: nextLink,
+                previous: previousLink,
+            },
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
