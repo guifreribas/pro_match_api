d4cfb765e5308e0ec77176597dee3ed9e5fb085c
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 21:31:15 +0200

    feature: add filters on getCompetitions

--- src/controllers/competitionController.js
+++ src/controllers/competitionController.js
@@ -1,12 +1,61 @@
 import Competition from "../models/competitionModel.js";
+import config from "../config/config.js";
 
 export const getCompetitions = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const whereConditions = {};
+    if (req.query.type) {
+        whereConditions.type = req.query.type;
+    }
+    if (req.query.subtype) {
+        whereConditions.subtype = req.query.subtype;
+    }
+
     try {
-        const competitions = await Competition.findAll();
+        const { count, rows } = await Competition.findAndCountAll({
+            where: whereConditions,
+            offset,
+            limit,
+        });
+        const totalPages = Math.ceil(count / limit);
+        const previousLink =
+            page > 1
+                ? `${config.API_BASE_URL}/competitions?page=${page - 1}`
+                : null;
+        const nextLink =
+            page < totalPages
+                ? `${config.API_BASE_URL}/competitions?page=${page + 1}`
+                : null;
+
         res.status(200).json({
             success: true,
             message: "Competitions fetched successfully",
-            data: competitions,
+            data: {
+                items: rows.map((competition) => ({
+                    id_competition: competition.id_competition,
+                    type: competition.type,
+                    subtype: competition.subtype,
+                    createdAt: competition.createdAt,
+                    updatedAt: competition.updatedAt,
+                })),
+                itemCount: rows.length,
+                totalItems: count,
+                currentPage: page,
+                pageSize: limit,
+                totalPages: totalPages,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+            links: {
+                self: `${config.API_BASE_URL}/competitions?page=${page}`,
+                first: `${config.API_BASE_URL}/competitions?page=1`,
+                last: `${config.API_BASE_URL}/competitions?page=${totalPages}`,
+                next: nextLink,
+                previous: previousLink,
+            },
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
