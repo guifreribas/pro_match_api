4b15ce27833a5491a62e352e16a7b1be695c84cc
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 21:32:38 +0200

    feature: add filters on getPlayers

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,12 +1,88 @@
 import Player from "../models/playerModel.js";
+import config from "../config/config.js";
 
 export const getPlayers = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const whereConditions = {};
+    if (req.query.name) {
+        whereConditions.name = req.query.name;
+    }
+    if (req.query.last_name) {
+        whereConditions.last_name = req.query.last_name;
+    }
+    if (req.query.dni) {
+        whereConditions.dni = req.query.dni;
+    }
+    if (req.query.avatar) {
+        whereConditions.avatar = req.query.avatar;
+    }
+    // Can filter by birthday. Format: YYYY-MM-DD. Example:
+    //  // ?birthdayBefore=2000-01-01
+    //  // ?birthdayAfter=2000-01-01
+    //  // ?birthdayEqual=2000-01-01
+
+    if (req.query.birthdayBefore) {
+        whereConditions.birthday = {
+            ...whereConditions.birthday,
+            [Op.lt]: new Date(req.query.birthdayBefore),
+        };
+    }
+    if (req.query.birthdayAfter) {
+        whereConditions.birthday = {
+            ...whereConditions.birthday,
+            [Op.gt]: new Date(req.query.birthdayAfter),
+        };
+    }
+    if (req.query.birthdayEqual) {
+        whereConditions.birthday = {
+            ...whereConditions.birthday,
+            [Op.eq]: new Date(req.query.birthdayEqual),
+        };
+    }
     try {
-        const players = await Player.findAll();
+        const { count, rows } = await Player.findAndCountAll({
+            where: whereConditions,
+            offset,
+            limit,
+        });
+        const totalPages = Math.ceil(count / limit);
+        const previousLink =
+            page > 1 ? `${config.API_BASE_URL}/players?page=${page - 1}` : null;
+        const nextLink =
+            page < totalPages
+                ? `${config.API_BASE_URL}/players?page=${page + 1}`
+                : null;
+
         res.status(200).json({
             success: true,
             message: "Players fetched successfully",
-            data: players,
+            data: {
+                items: rows.map((player) => ({
+                    id_player: player.id_player,
+                    name: player.name,
+                    last_name: player.last_name,
+                    dni: player.dni,
+                    avatar: player.avatar,
+                    birthday: player.birthday,
+                })),
+                itemCount: rows.length,
+                totalItems: count,
+                currentPage: page,
+                pageSize: limit,
+                totalPages: totalPages,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+            links: {
+                self: `${config.API_BASE_URL}/players?page=${page}`,
+                first: `${config.API_BASE_URL}/players?page=1`,
+                last: `${config.API_BASE_URL}/players?page=${totalPages}`,
+                next: nextLink,
+                previous: previousLink,
+            },
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
