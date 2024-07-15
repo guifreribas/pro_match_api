ddb6d47cb507619b123de1965d7f0b3880526a53
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 21:31:59 +0200

    features: add filters on getGoals

--- src/controllers/goalController.js
+++ src/controllers/goalController.js
@@ -2,13 +2,68 @@ import config from "../config/config.js";
 import Goal from "../models/goalModel.js";
 
 export const getGoals = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const whereConditions = {};
+    if (req.query.minute) {
+        whereConditions.minute = req.query.minute;
+    }
+    if (req.query.part) {
+        whereConditions.part = req.query.part;
+    }
+    if (req.query.player_id) {
+        whereConditions.player_id = req.query.player_id;
+    }
+    if (req.query.team_id) {
+        whereConditions.team_id = req.query.team_id;
+    }
+    if (req.query.match_id) {
+        whereConditions.match_id = req.query.match_id;
+    }
+
     try {
-        const goals = await Goal.findAll();
-        console.log({ goals });
+        const { count, rows } = await Goal.findAndCountAll({
+            where: whereConditions,
+            offset,
+            limit,
+        });
+        const totalPages = Math.ceil(count / limit);
+        const previousLink =
+            page > 1 ? `${config.API_BASE_URL}/goals?page=${page - 1}` : null;
+        const nextLink =
+            page < totalPages
+                ? `${config.API_BASE_URL}/goals?page=${page + 1}`
+                : null;
+
         res.status(200).json({
             success: true,
             message: "Goals fetched successfully",
-            data: goals,
+            data: {
+                items: rows.map((goal) => ({
+                    id_goal: goal.id_goal,
+                    minute: goal.minute,
+                    part: goal.part,
+                    player_id: goal.player_id,
+                    team_id: goal.team_id,
+                    match_id: goal.match_id,
+                })),
+                itemCount: rows.length,
+                totalItems: count,
+                currentPage: page,
+                pageSize: limit,
+                totalPages: totalPages,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+            links: {
+                self: `${config.API_BASE_URL}/goals?page=${page}`,
+                first: `${config.API_BASE_URL}/goals?page=1`,
+                last: `${config.API_BASE_URL}/goals?page=${totalPages}`,
+                next: nextLink,
+                previous: previousLink,
+            },
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
