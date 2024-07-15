5bf9cfe4f7a3df5929dcb89346e98086e5a390d8
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 11:15:47 +0200

    db file

---
+++ src/db.js
@@ -0,0 +1,43 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import config from "./config/config.js";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    // config.database.database,
+    // config.database.username,
+    // config.database.password,
+    process.env.DATABASE,
+    process.env.USER_NAME,
+    process.env.PASSWORD,
+    {
+        // host: config.database.host,
+        host: process.env.HOST_NAME,
+        dialect: "mysql",
+    }
+);
+
+const syncroModel = async () => {
+    try {
+        // Sincronizar el modelo con la base de datos (crear la tabla si no existe)
+        // Con "alter: true" se sincronizan las columnas y se crean/eliminan si fuera necesario
+        await sequelize.sync({ force: false }).then(() => {
+            console.log("Modelos sincronizado con la base de datos");
+        });
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+        await syncroModel();
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+export { sequelize, testConnection };
