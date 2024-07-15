7368242142d311d48a12061fe4403e247bcc741c
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-15 11:16:10 +0200

    index file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
+import config from "./config/config.js";
 
-dotenv.config();
+import usersRouter from "../src/routes/usersRoutes.js";
+import authRouter from "../src/routes/authRoutes.js";
+import cardsRouter from "../src/routes/cardsRoutes.js";
+import categoriesRouter from "../src/routes/categoriesRoutes.js";
+import matchesRouter from "../src/routes/matchesRoutes.js";
+import organizationsRouter from "../src/routes/organizationsRoutes.js";
+import playersRouter from "../src/routes/playersRoutes.js";
+import resultsRouter from "../src/routes/resultsRoutes.js";
+import sportsRouter from "../src/routes/sportsRoutes.js";
+import teamsRouter from "../src/routes/teamsRoutes.js";
+import goalRouter from "../src/routes/goalRoutes.js";
+import cardRouter from "../src/routes/cardsRoutes.js";
+import foulRouter from "../src/routes/foulRoutes.js";
+import competitionsRoutes from "./routes/competitionsRoutes.js";
 
 const app = express();
 
@@ -18,4 +31,19 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server starteD!"));
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/cards", cardsRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/competition", competitionsRoutes);
+app.use("/api/matches", matchesRouter);
+app.use("/api/organizations", organizationsRouter);
+app.use("/api/players", playersRouter);
+app.use("/api/results", resultsRouter);
+app.use("/api/sports", sportsRouter);
+app.use("/api/teams", teamsRouter);
+app.use("/api/goals", goalRouter);
+app.use("/api/card", cardRouter);
+app.use("/api/fouls", foulRouter);
+
+app.listen(config.port, () => console.log("Server starteD!", config.port));
