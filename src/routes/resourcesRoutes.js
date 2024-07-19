588e34713db62c285b350b51513423176fa9eab0
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-19 15:52:48 +0200

    fix: imported authenticateToken

--- src/routes/resourcesRoutes.js
+++ src/routes/resourcesRoutes.js
@@ -4,6 +4,7 @@ import {
     deleteResource,
 } from "../controllers/resourceController.js";
 import parser from "../config/multerConfig.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = Router();
 const allRoles = ["ADMIN", "SUPER_ADMIN", "USER"];
