6f6039ba923049f31ea860f6a7bc48c37c5881c7
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-23 11:33:15 +0200

    feat: add getResources and getResource routes on resources routes

--- src/routes/resourcesRoutes.js
+++ src/routes/resourcesRoutes.js
@@ -2,12 +2,17 @@ import { Router } from "express";
 import {
     createResource,
     deleteResource,
+    getResource,
+    getResources,
 } from "../controllers/resourceController.js";
 import parser from "../config/multerConfig.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = Router();
 const allRoles = ["ADMIN", "SUPER_ADMIN", "USER"];
+
+router.get("/", authenticateToken(allRoles), getResources);
+router.get("/:id", authenticateToken(allRoles), getResource);
 
 router.post(
     "/",
