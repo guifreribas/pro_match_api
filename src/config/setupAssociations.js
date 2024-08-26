b542ed34b1e2390cab8220b1713b379ee0061d75
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-26 07:23:36 +0200

    feat: add setupAssociations function to define model relationships and include it in index.js for initialization of associations

---
+++ src/config/setupAssociations.js
@@ -0,0 +1,73 @@
+import Card from "#models/cardModel.js";
+import Category from "#models/categoryModel.js";
+import Competition from "#models/competitionModel.js";
+import Field from "#models/fieldModel.js";
+import Foul from "#models/foulModel.js";
+import Goal from "#models/goalModel.js";
+import Match from "#models/matchModel.js";
+import Organization from "#models/organizationModel.js";
+import Player from "#models/playerModel.js";
+import Result from "#models/resultModel.js";
+import Team from "#models/teamModel.js";
+import TeamPlayer from "#models/teamPlayerModel.js";
+import User from "#models/userModel.js";
+
+export function setupAssociations() {
+    Card.belongsTo(Match, { foreignKey: "match_id" });
+    Card.belongsTo(Player, { foreignKey: "player_id" });
+    Card.belongsTo(Team, { foreignKey: "team_id" });
+
+    Category.belongsTo(Organization, { foreignKey: "organization_id" });
+    Category.belongsTo(User, { foreignKey: "user_id" });
+    Category.belongsTo(Competition, { foreignKey: "competition_id" });
+
+    Competition.hasMany(Category, { foreignKey: "competition_id" });
+    Competition.hasMany(Field, { foreignKey: "competition_id" });
+    Competition.hasMany(Organization, { foreignKey: "competition_id" });
+
+    Field.belongsTo(Organization, { foreignKey: "organization_id" });
+    Field.belongsTo(Competition, { foreignKey: "competition_id" });
+    Field.belongsTo(User, { foreignKey: "user_id" });
+
+    Foul.belongsTo(Match, { foreignKey: "match_id" });
+    Foul.belongsTo(Player, { foreignKey: "player_id" });
+    Foul.belongsTo(Team, { foreignKey: "team_id" });
+
+    Goal.belongsTo(Match, { foreignKey: "match_id" });
+    Goal.belongsTo(Player, { foreignKey: "player_id" });
+    Goal.belongsTo(Team, { foreignKey: "team_id" });
+
+    Match.belongsTo(Category, { foreignKey: "category_id" });
+    Match.belongsTo(Team, { foreignKey: "local_team" });
+    Match.belongsTo(Team, { foreignKey: "visitor_team" });
+    Match.hasMany(Card, { foreignKey: "match_id" });
+    Match.hasMany(Foul, { foreignKey: "match_id" });
+    Match.hasMany(Goal, { foreignKey: "match_id" });
+    Match.hasMany(Result, { foreignKey: "match_id" });
+
+    Organization.belongsTo(Competition, { foreignKey: "competition_id" });
+    Organization.belongsTo(User, { foreignKey: "user_id" });
+    Organization.hasMany(Category, { foreignKey: "organization_id" });
+
+    Result.belongsTo(Match, { foreignKey: "match_id" });
+    Match.hasOne(Result, { foreignKey: "match_id" });
+    Match.hasMany(Goal, { foreignKey: "match_id" });
+
+    Player.belongsTo(User, { foreignKey: "user_id" });
+    Player.hasMany(TeamPlayer, { foreignKey: "player_id" });
+    Player.hasMany(Card, { foreignKey: "player_id" });
+    Player.hasMany(Foul, { foreignKey: "player_id" });
+    Player.hasMany(Goal, { foreignKey: "player_id" });
+
+    Team.belongsTo(Category, { foreignKey: "category_id" });
+    Team.belongsTo(Organization, { foreignKey: "organization_id" });
+    Team.hasMany(TeamPlayer, { foreignKey: "team_id" });
+
+    TeamPlayer.belongsTo(Team, { foreignKey: "team_id" });
+    TeamPlayer.belongsTo(Player, { foreignKey: "player_id" });
+    TeamPlayer.belongsTo(User, { foreignKey: "user_id" });
+
+    User.hasMany(Category, { foreignKey: "user_id" });
+    User.hasMany(Organization, { foreignKey: "user_id" });
+    User.hasMany(Player, { foreignKey: "user_id" });
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import config from "./config/config.js";
+import { setupAssociations } from "./config/setupAssociations.js";
 
 import authRouter from "../src/routes/authRoutes.js";
 import cardRouter from "../src/routes/cardsRoutes.js";
@@ -19,6 +20,7 @@ import resultsRouter from "../src/routes/resultsRoutes.js";
 import teamsRouter from "../src/routes/teamsRoutes.js";
 import usersRouter from "../src/routes/usersRoutes.js";
 import fieldsRouter from "../src/routes/fieldsRoutes.js";
+import teamsPlayersRouter from "../src/routes/teamsPlayersRoutes.js";
 
 const app = express();
 
@@ -46,6 +48,9 @@ app.use("/api/players", playersRouter);
 app.use("/api/resources", resourcesRouter);
 app.use("/api/results", resultsRouter);
 app.use("/api/teams", teamsRouter);
+app.use("/api/team-players", teamsPlayersRouter);
 app.use("/api/users", usersRouter);
 
 app.listen(config.port, () => console.log("Server started", config.port));
+
+setupAssociations();
