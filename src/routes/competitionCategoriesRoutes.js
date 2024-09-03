a7b5922944b80954221af63e3d0027e4cf6ded05
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-03 09:50:50 +0200

    feat: add competitionCategoriesRoutes for full CRUD operations with authentication for managing competition categories

---
+++ src/routes/competitionCategoriesRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+    createCompetitionCategory,
+    deleteCompetitionCategory,
+    getCompetitionCategories,
+    getCompetitionCategory,
+    updateCompetitionCategory,
+} from "../controllers/competitionCategoryController.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js";
+
+const router = Router();
+const allRoles = ["ADMIN", "SUPER_ADMIN", "USER"];
+
+router.get("/", authenticateToken(allRoles), getCompetitionCategories);
+router.get("/:id", authenticateToken(allRoles), getCompetitionCategory);
+router.post("/", authenticateToken(allRoles), createCompetitionCategory);
+router.put("/:id", authenticateToken(allRoles), updateCompetitionCategory);
+router.delete("/:id", authenticateToken(allRoles), deleteCompetitionCategory);
+
+export default router;
