4d8eabaf1bfeb97dcf8c3a19290c8b91dfaece0d
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-21 07:03:07 +0200

    refactor: add user_id field to Foul model and enforce required fields validation in createFoul controller for data integrity

--- src/controllers/foulController.js
+++ src/controllers/foulController.js
@@ -2,163 +2,178 @@ import Foul from "../models/foulModel.js";
 import config from "../config/config.js";
 
 export const getFouls = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
+	const page = parseInt(req.query.page) || 1;
+	const limit = parseInt(req.query.limit) || 10;
+	const offset = (page - 1) * limit;
 
-    const whereConditions = {};
-    if (req.query.minute) {
-        whereConditions.minute = req.query.minute;
-    }
-    if (req.query.part) {
-        whereConditions.part = req.query.part;
-    }
-    if (req.query.player_id) {
-        whereConditions.player_id = req.query.player_id;
-    }
-    if (req.query.team_id) {
-        whereConditions.team_id = req.query.team_id;
-    }
-    if (req.query.match_id) {
-        whereConditions.match_id = req.query.match_id;
-    }
-    try {
-        const { count, rows } = await Foul.findAndCountAll({
-            where: whereConditions,
-            offset,
-            limit,
-        });
-        const totalPages = Math.ceil(count / limit);
-        const previousLink =
-            page > 1 ? `${config.API_BASE_URL}/fouls?page=${page - 1}` : null;
-        const nextLink =
-            page < totalPages
-                ? `${config.API_BASE_URL}/fouls?page=${page + 1}`
-                : null;
+	const whereConditions = {};
+	if (req.query.minute) {
+		whereConditions.minute = req.query.minute;
+	}
+	if (req.query.part) {
+		whereConditions.part = req.query.part;
+	}
+	if (req.query.player_id) {
+		whereConditions.player_id = req.query.player_id;
+	}
+	if (req.query.team_id) {
+		whereConditions.team_id = req.query.team_id;
+	}
+	if (req.query.match_id) {
+		whereConditions.match_id = req.query.match_id;
+	}
+	try {
+		const { count, rows } = await Foul.findAndCountAll({
+			where: whereConditions,
+			offset,
+			limit,
+		});
+		const totalPages = Math.ceil(count / limit);
+		const previousLink =
+			page > 1 ? `${config.API_BASE_URL}/fouls?page=${page - 1}` : null;
+		const nextLink =
+			page < totalPages
+				? `${config.API_BASE_URL}/fouls?page=${page + 1}`
+				: null;
 
-        res.status(200).json({
-            success: true,
-            message: "Fouls fetched successfully",
-            data: {
-                items: rows.map((foul) => ({
-                    id_foul: foul.id_foul,
-                    minute: foul.minute,
-                    part: foul.part,
-                    player_id: foul.player_id,
-                    team_id: foul.team_id,
-                    match_id: foul.match_id,
-                    createdAt: foul.createdAt,
-                    updatedAt: foul.updatedAt,
-                })),
-                itemCount: rows.length,
-                totalItems: count,
-                currentPage: page,
-                pageSize: limit,
-                totalPages: totalPages,
-                hasNextPage: page < totalPages,
-                hasPreviousPage: page > 1,
-            },
-            links: {
-                self: `${config.API_BASE_URL}/fouls?page=${page}`,
-                first: `${config.API_BASE_URL}/fouls?page=1`,
-                last: `${config.API_BASE_URL}/fouls?page=${totalPages}`,
-                next: nextLink,
-                previous: previousLink,
-            },
-            timestamp: new Date().toISOString(),
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Error to get fouls" });
-    }
+		res.status(200).json({
+			success: true,
+			message: "Fouls fetched successfully",
+			data: {
+				items: rows.map((foul) => ({
+					id_foul: foul.id_foul,
+					minute: foul.minute,
+					part: foul.part,
+					player_id: foul.player_id,
+					team_id: foul.team_id,
+					match_id: foul.match_id,
+					createdAt: foul.createdAt,
+					updatedAt: foul.updatedAt,
+				})),
+				itemCount: rows.length,
+				totalItems: count,
+				currentPage: page,
+				pageSize: limit,
+				totalPages: totalPages,
+				hasNextPage: page < totalPages,
+				hasPreviousPage: page > 1,
+			},
+			links: {
+				self: `${config.API_BASE_URL}/fouls?page=${page}`,
+				first: `${config.API_BASE_URL}/fouls?page=1`,
+				last: `${config.API_BASE_URL}/fouls?page=${totalPages}`,
+				next: nextLink,
+				previous: previousLink,
+			},
+			timestamp: new Date().toISOString(),
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Error to get fouls" });
+	}
 };
 
 export const getFoul = async (req, res) => {
-    try {
-        const foul = await Foul.findByPk(req.params.id);
-        if (foul) {
-            res.status(200).json({
-                success: true,
-                message: "Foul fetched successfully",
-                data: foul,
-                timestamp: new Date().toISOString(),
-            });
-        } else {
-            res.status(404).json({
-                success: false,
-                message: "Foul not found",
-                data: {
-                    id_foul: req.params.id,
-                },
-                timestamp: new Date().toISOString(),
-            });
-        }
-    } catch (error) {
-        res.status(500).json({ error: "Error to get foul" });
-    }
+	try {
+		const foul = await Foul.findByPk(req.params.id);
+		if (foul) {
+			res.status(200).json({
+				success: true,
+				message: "Foul fetched successfully",
+				data: foul,
+				timestamp: new Date().toISOString(),
+			});
+		} else {
+			res.status(404).json({
+				success: false,
+				message: "Foul not found",
+				data: {
+					id_foul: req.params.id,
+				},
+				timestamp: new Date().toISOString(),
+			});
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Error to get foul" });
+	}
 };
 
 export const createFoul = async (req, res) => {
-    try {
-        const foul = await Foul.create(req.body);
-        res.status(201).json({
-            success: true,
-            message: "Foul created successfully",
-            data: foul,
-            links: {
-                self: `${config.API_BASE_URL}/fouls/${foul.id_foul}`,
-            },
-            timestamp: new Date().toISOString(),
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error to create foul" });
-    }
+	if (
+		!req.body.minute ||
+		!req.body.part ||
+		!req.body.player_id ||
+		!req.body.team_id ||
+		!req.body.match_id ||
+		!req.body.user_id
+	) {
+		return res.status(400).json({
+			success: false,
+			message: "Missing required fields",
+			data: null,
+			timestamp: new Date().toISOString(),
+		});
+	}
+	try {
+		const foul = await Foul.create(req.body);
+		res.status(201).json({
+			success: true,
+			message: "Foul created successfully",
+			data: foul,
+			links: {
+				self: `${config.API_BASE_URL}/fouls/${foul.id_foul}`,
+			},
+			timestamp: new Date().toISOString(),
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Error to create foul" });
+	}
 };
 
 export const updateFoul = async (req, res) => {
-    try {
-        const foul = await Foul.update(req.body, {
-            where: { id_foul: req.params.id },
-        });
-        res.status(200).json({
-            success: true,
-            message: "Foul updated successfully",
-            data: foul,
-            links: {
-                self: `${config.API_BASE_URL}/fouls/${foul.id_foul}`,
-            },
-            timestamp: new Date().toISOString(),
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error to update foul" });
-    }
+	try {
+		const foul = await Foul.update(req.body, {
+			where: { id_foul: req.params.id },
+		});
+		res.status(200).json({
+			success: true,
+			message: "Foul updated successfully",
+			data: foul,
+			links: {
+				self: `${config.API_BASE_URL}/fouls/${foul.id_foul}`,
+			},
+			timestamp: new Date().toISOString(),
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Error to update foul" });
+	}
 };
 
 export const deleteFoul = async (req, res) => {
-    try {
-        const foul = await Foul.destroy({
-            where: { id_foul: req.params.id },
-        });
-        if (foul) {
-            res.status(200).json({
-                success: true,
-                message: "Foul deleted successfully",
-                data: {
-                    id: req.params.id,
-                },
-                timestamp: new Date().toISOString(),
-            });
-        } else {
-            res.status(404).json({
-                success: false,
-                message: "Foul not found",
-                data: {
-                    id: req.params.id,
-                },
-                timestamp: new Date().toISOString(),
-            });
-        }
-    } catch (error) {
-        res.status(500).json({ error: "Error to delete foul" });
-    }
+	try {
+		const foul = await Foul.destroy({
+			where: { id_foul: req.params.id },
+		});
+		if (foul) {
+			res.status(200).json({
+				success: true,
+				message: "Foul deleted successfully",
+				data: {
+					id: req.params.id,
+				},
+				timestamp: new Date().toISOString(),
+			});
+		} else {
+			res.status(404).json({
+				success: false,
+				message: "Foul not found",
+				data: {
+					id: req.params.id,
+				},
+				timestamp: new Date().toISOString(),
+			});
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Error to delete foul" });
+	}
 };
--- src/models/foulModel.js
+++ src/models/foulModel.js
@@ -2,45 +2,55 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../db.js";
 
 const Foul = sequelize.define(
-    "foul",
-    {
-        id_foul: {
-            type: DataTypes.INTEGER(10).UNSIGNED,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        minute: {
-            type: DataTypes.INTEGER(3).UNSIGNED,
-            allowNull: false,
-        },
-        part: {
-            type: DataTypes.ENUM(
-                "FIRST_HALF",
-                "SECOND_HALF",
-                "EXTRA_TIME_FIRST_HALF",
-                "EXTRA_TIME_SECOND_HALF",
-                "PENALTIES"
-            ),
-            allowNull: false,
-        },
-        player_id: {
-            type: DataTypes.INTEGER(10).UNSIGNED,
-            allowNull: false,
-        },
-        team_id: {
-            type: DataTypes.INTEGER(10).UNSIGNED,
-            allowNull: false,
-        },
-        match_id: {
-            type: DataTypes.INTEGER(10).UNSIGNED,
-            allowNull: false,
-        },
-    },
-    {
-        timestamps: true,
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-    }
+	"foul",
+	{
+		id_foul: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		minute: {
+			type: DataTypes.INTEGER(3).UNSIGNED,
+			allowNull: false,
+			required: true,
+		},
+		part: {
+			type: DataTypes.ENUM(
+				"FIRST_HALF",
+				"SECOND_HALF",
+				"EXTRA_TIME_FIRST_HALF",
+				"EXTRA_TIME_SECOND_HALF",
+				"PENALTIES"
+			),
+			allowNull: false,
+			required: true,
+		},
+		player_id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			required: true,
+		},
+		team_id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			required: true,
+		},
+		match_id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			required: true,
+		},
+		user_id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+		createdAt: "created_at",
+		updatedAt: "updated_at",
+	}
 );
 
 // Foul.belongsTo(Match, { foreignKey: "match_id" });
