05829f8088bc6048b03369341f372702650465fe
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-25 17:41:53 +0200

    feat: remove authentication from getOrganizations route for improved accessibility to organization list

--- src/routes/organizationsRoutes.js
+++ src/routes/organizationsRoutes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import {
-    createOrganization,
-    deleteOrganization,
-    getOrganization,
-    getOrganizations,
-    updateOrganization,
+	createOrganization,
+	deleteOrganization,
+	getOrganization,
+	getOrganizations,
+	updateOrganization,
 } from "../controllers/organizationController.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = Router();
 const allRoles = ["ADMIN", "SUPER_ADMIN", "USER"];
 
-router.get("/", authenticateToken(allRoles), getOrganizations);
+router.get("/", getOrganizations);
 router.get("/:id", authenticateToken(allRoles), getOrganization);
 router.post("/", authenticateToken(allRoles), createOrganization);
 router.put("/:id", authenticateToken(allRoles), updateOrganization);
