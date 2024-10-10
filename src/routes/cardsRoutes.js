264fc64eff3a53b85a09c8eed401de4a1cad8947
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-10 10:20:35 +0200

    feat: add /cards-stats route to cardsRoutes for retrieving player card statistics, improving API accessibility and user insights

--- src/routes/cardsRoutes.js
+++ src/routes/cardsRoutes.js
@@ -5,6 +5,7 @@ import {
 	createCard,
 	updateCard,
 	deleteCard,
+	getPlayerCardsStats,
 } from "../controllers/cardController.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 
@@ -12,6 +13,7 @@ const router = Router();
 const allRoles = ["ADMIN", "SUPER_ADMIN", "USER"];
 
 router.get("/", getCards);
+router.get("/cards-stats", getPlayerCardsStats);
 router.get("/:id", getCard);
 router.post("/", authenticateToken(allRoles), createCard);
 router.put("/:id", authenticateToken(allRoles), updateCard);
